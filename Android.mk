fc4d28220790fe41cc292d02a846a91d6333a437
sattarvoybek <sattarvoybek>
2017-12-25 20:31:15 +0300

    fix arch arm 32

--- Android.mk
+++ Android.mk
@@ -225,9 +225,9 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := vendor.qti.hardware.fm@1.0
-LOCAL_MODULE_OWNER := xiaomi
+LOCAL_MODULE_OWNER := nubia
 LOCAL_SRC_FILES := proprietary/lib/vendor.qti.hardware.fm@1.0.so
-LOCAL_MULTILIB := 32
+LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
